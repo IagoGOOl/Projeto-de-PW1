3edb3f3abbfbd60247e9cb994bfb008f2f0f6c96
PauloSantiagoADS <paulosantiago8@gmail.com>
2023-12-11 13:31:36 -0300

    Atualizando os arquivos

--- Projeto PW1/src/Controllers/controllerUse.ts
+++ Projeto PW1/src/Controllers/controllerUse.ts
@@ -1,23 +1,23 @@
-import { Request, Response } from 'express';
-import { prismaService } from '../Service/prismaService';
-import { PrismaClient, Prisma } from '@prisma/client';
+import { Request, Response } from "express";
+import { prismaService } from "../Service/prismaService";
+import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export class UserController {
-	async create(req: Request, res: Response) {
-		const { nome, email, password } = req.body;
+  async create(req: Request, res: Response) {
+    const { name, email, password } = req.body;
 
-		try {
-			await prismaService.user.create({
-				data: {
-					nome,
-					email,
-					password,
-				},
-			});
-			res.status(201).json({ message: 'Usuario Criado com sucesso' });
-		} catch (err) {
-			res.status(500).json({ message: 'Erro ao criar o Usuario' });
-		}
-	}
+    try {
+      await prismaService.user.create({
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+      res.status(201).json({ message: "Usuario Criado com sucesso" });
+    } catch (err) {
+      res.status(500).json({ message: "Erro ao criar o Usuario" });
+    }
+  }
 }
--- Projeto PW1/src/Service/prismaService.ts
+++ Projeto PW1/src/Service/prismaService.ts
@@ -1,3 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 export const prismaService = new PrismaClient();
+
+async function main() {
+  await prismaService.user.create({
+    data: {
+      name: "Igor ads",
+      email: "igor@gmail.com",
+      password: "BomNoite",
+    },
+  });
+}
+
+main();
