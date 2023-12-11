3b43f3b05b65c996f695eaeb59f0cca73e978406
PauloSantiagoADS <paulosantiago8@gmail.com>
2023-12-11 15:35:27 -0300

    Atualizando o gitignore e prismaService

--- Projeto PW1/src/Service/prismaService.ts
+++ Projeto PW1/src/Service/prismaService.ts
@@ -1,15 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prismaService = new PrismaClient();
-
-async function main() {
-  await prismaService.user.create({
-    data: {
-      name: "Igor ads",
-      email: "igor@gmail.com",
-      password: "BomNoite",
-    },
-  });
-}
-
-main();
