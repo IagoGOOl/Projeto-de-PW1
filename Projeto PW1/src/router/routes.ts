cd6792dd41fdf8dacbf5c37cf64a95d28cf894fd
WarllaK <warlla.kelly1@gmail.com>
2023-12-13 08:54:35 -0300

    Adicionando tabela 'comments', rotas e seu respectivo controller

--- Projeto PW1/src/router/routes.ts
+++ Projeto PW1/src/router/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { UserController } from '../Controllers/controllerUse';
+import { UserController } from '../Controllers/controllerUser';
 import { PostController } from '../Controllers/controllerPost';
+import { CommentController } from '../Controllers/controllerComment';
 
 const router = Router();
 
@@ -8,11 +9,19 @@ const userController = new UserController();
 
 const postController = new PostController();
 
+const commentController = new CommentController();
 
 router.post('/user', userController.create);
 
 // Rotas da Postagem
 router.post('/post', postController.create);
 router.get('/post', postController.readAll);
+
+// Rotas dos Comentários da Postagem
+router.post('/post/:postId/comment', commentController.create);
+router.get('/post/:postId/comment', commentController.readAll);
+router.get('/post/:postId/comment/:id', commentController.read);
+router.put('/post/:postId/comment/:id', commentController.update);
+router.delete('/post/:postId/comment/:id', commentController.delete);
 
 export default router;
