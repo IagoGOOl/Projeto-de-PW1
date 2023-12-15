2f3edb377232b5426feadf09edfc40dea1d913de
IagoGOOl <iago.gool@gmail.com>
2023-12-15 11:15:16 -0300

    autenticação de usuario

--- Projeto PW1/src/Controllers/controllerUse.ts
+++ Projeto PW1/src/Controllers/controllerUse.ts
@@ -5,17 +5,21 @@ import { PrismaClient, Prisma } from '@prisma/client';
 const prisma = new PrismaClient();
 export class UserController {
 	async create(req: Request, res: Response) {
-		const { nome, email, password } = req.body;
-
+		const { name, email, password } = req.body;
+		const userExists = await prisma.user.findUnique({ where: { email } });
+		if (userExists) {
+			return res.json({ error: 'usuario existir' });
+		}
 		try {
-			await prismaService.user.create({
+			const user = await prismaService.user.create({
 				data: {
-					nome,
+					name,
 					email,
 					password,
 				},
 			});
-			res.status(201).json({ message: 'Usuario Criado com sucesso' });
+			res.status(201).json({ messagem: 'Usuario Criado com Sucesso' });
+			return res.json({ user });
 		} catch (err) {
 			res.status(500).json({ message: 'Erro ao criar o Usuario' });
 		}
