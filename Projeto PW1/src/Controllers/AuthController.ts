dfc7a99d7cd2ecf8896e4a0b9e18f7c773f0a195
PauloSantiagoADS <paulosantiago8@gmail.com>
2023-12-16 09:26:25 -0300

    atualizando

--- Projeto PW1/src/Controllers/AuthController.ts
+++ Projeto PW1/src/Controllers/AuthController.ts
@@ -1,23 +1,26 @@
-import { Request, Response } from 'express';
-import { prisma } from '../utils/prisma';
-import { compare } from 'bcrypt';
-import { sign } from 'jsonwebtoken';
+import { Request, Response } from "express";
+import { prisma } from "../utils/prisma";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
 export class AuteticaControleer {
-	async auteticacao(req: Request, res: Response) {
-		const { email, password } = req.body;
-		const user = await prisma.user.findUnique({ where: { email } });
-		if (!user) {
-			return res.json({ error: 'Email inválido' });
-		}
-		const isValuePassword = await compare(password, user.password);
-		if (!isValuePassword) {
-			return res.json({ error: 'senha invalida' });
-		}
-		const token = sign({ id: user.id }, 'chaveSecreta', {
-			expiresIn: '1d',
-		});
+  async auteticacao(req: Request, res: Response) {
+    const { email, password } = req.body;
 
-		const { id } = user;
-		return res.json({});
-	}
+    //validar
+
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.json({ error: "Email inválido" });
+    }
+    const isValuePassword = await compare(password, user.password);
+    if (!isValuePassword) {
+      return res.json({ error: "senha invalida" });
+    }
+    const token = sign({ id: user.id }, "chaveSecreta", {
+      expiresIn: "1d",
+    });
+
+    const { id } = user;
+    return res.json({ token });
+  }
 }
