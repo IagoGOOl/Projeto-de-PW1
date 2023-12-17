22c3d50d2fd7ffd3bcb71b12c72c3dbf739820ee
WarllaK <warlla.kelly1@gmail.com>
2023-12-17 17:38:52 -0300

    adicionando autenticação no comentario

--- Projeto PW1/src/Controllers/controllerComment.ts
+++ Projeto PW1/src/Controllers/controllerComment.ts
@@ -1,233 +1,196 @@
-import { Request, Response } from 'express';
-import { prismaService } from '../service/prismaService';
+import { Request, Response } from "express";
+import { prismaService } from "../service/prismaService";
 
 function response(res: Response, message: string, status: number = 400) {
-	return res.status(status).json({ message });
+  return res.status(status).json({ message });
 }
 
 export class CommentController {
-	async create(req: Request, res: Response) {
-		const { postId } = req.params;
-		const { description, email } = req.body;
+  async create(req: Request, res: Response) {
+    const userId = req.userID;
+    const { postId } = req.params;
+    const { description } = req.body;
 
-		if (!postId) {
-			return response(
-				res,
-				'O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!',
-				404
-			);
-		}
+    if (!userId) {
+      return response(
+        res,
+        "usuario não autorizado, Faça login e tente novamente!"
+      );
+    }
 
-		if (!description) {
-			return response(res, 'Não é possível criar um comentário vazio');
-		}
+    if (!postId) {
+      return response(
+        res,
+        "O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!"
+      );
+    }
 
-		if (!email) {
-			return response(
-				res,
-				'Cadê sua identificação? Faça login e tente novamente!'
-			);
-		}
+    if (!description) {
+      return response(res, "Não é possível criar um comentário vazio");
+    }
 
-		const userExists = await prismaService.user.findFirst({
-			where: {
-				email,
-			},
-		});
+    try {
+      await prismaService.comment.create({
+        data: {
+          description,
+          postId: Number(postId),
+          authorId: userId,
+        },
+      });
+      response(res, "Seu comentário foi salvo com sucesso", 200);
+    } catch (err) {
+      response(res, "Erro ao criar comentário", 500);
+    }
+  }
 
-		if (!userExists) {
-			return response(
-				res,
-				'Ooops, parece que você não pode comentar nessa postagem. Faça login e tente novamente!'
-			);
-		}
+  async readAll(req: Request, res: Response) {
+    const { postId } = req.params;
 
-		try {
-			await prismaService.comment.create({
-				data: {
-					description,
-					postId: Number(postId),
-					authorId: userExists.id,
-				},
-			});
-			response(res, 'Seu comentário foi salvo com sucesso', 200);
-		} catch (err) {
-			response(res, 'Erro ao criar comentário', 500);
-		}
-	}
+    if (!postId) {
+      return response(
+        res,
+        "O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!"
+      );
+    }
 
-	async readAll(req: Request, res: Response) {
-		const { postId } = req.params;
+    try {
+      const comments = await prismaService.comment.findMany({
+        where: {
+          postId: Number(postId),
+        },
+      });
 
-		if (!postId) {
-			return response(
-				res,
-				'O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!'
-			);
-		}
+      res.status(200).json(comments);
+    } catch {
+      response(res, "Comentários não encontrados", 404);
+    }
+  }
 
-		try {
-			const comments = await prismaService.comment.findMany({
-				where: {
-					postId: Number(postId),
-				},
-			});
+  async read(req: Request, res: Response) {
+    const { postId, id: commentId } = req.params;
 
-			res.status(200).json(comments);
-		} catch {
-			response(res, 'Comentários não encontrados', 404);
-		}
-	}
+    if (!postId) {
+      return response(
+        res,
+        "O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!"
+      );
+    }
 
-	async read(req: Request, res: Response) {
-		const { postId, id } = req.params;
+    if (!commentId) {
+      return response(
+        res,
+        "O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!"
+      );
+    }
 
-		if (!postId) {
-			return response(
-				res,
-				'O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!'
-			);
-		}
+    try {
+      const comment = await prismaService.comment.findFirst({
+        where: {
+          id: Number(commentId),
+          postId: Number(postId),
+        },
+      });
 
-		if (!id) {
-			return response(
-				res,
-				'O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!'
-			);
-		}
+      res.status(200).json(comment);
+    } catch {
+      response(res, "Comentário não encontrado", 404);
+    }
+  }
 
-		try {
-			const comment = await prismaService.comment.findFirst({
-				where: {
-					id: Number(id),
-					postId: Number(postId),
-				},
-			});
+  async update(req: Request, res: Response) {
+    const userId = req.userID;
+    const { postId, id: commentId } = req.params;
+    const { description } = req.body;
 
-			res.status(200).json(comment);
-		} catch {
-			response(res, 'Comentário não encontrado', 404);
-		}
-	}
+    if (!userId) {
+      return response(res, "Você não pode atualizar esse comentário!", 403);
+    }
 
-	async update(req: Request, res: Response) {
-		const { postId, id } = req.params;
-		const { description, email } = req.body;
+    if (!postId) {
+      return response(
+        res,
+        "O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!"
+      );
+    }
 
-		if (!postId) {
-			return response(
-				res,
-				'O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!'
-			);
-		}
+    if (!commentId) {
+      return response(
+        res,
+        "O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!"
+      );
+    }
 
-		if (!id) {
-			return response(
-				res,
-				'O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!'
-			);
-		}
+    if (!description) {
+      return response(res, "Não é possível atualizar um comentário vazio");
+    }
 
-		if (!description) {
-			return response(
-				res,
-				'Não é possível atualizar um comentário vazio'
-			);
-		}
+    try {
+      await prismaService.comment.update({
+        where: {
+          id: Number(commentId),
+          postId: Number(postId),
+          authorId: userId,
+        },
+        data: {
+          description,
+        },
+      });
 
-		if (!email) {
-			return response(
-				res,
-				'Cadê sua identificação? Faça login e tente novamente!'
-			);
-		}
+      res.status(200).json({
+        message: "Comentário atualizado com sucesso",
+      });
+    } catch {
+      response(res, "Não foi possível atualizar o comentário", 403);
+    }
+  }
 
-		const userExists = await prismaService.user.findFirst({
-			where: {
-				email,
-			},
-		});
+  async delete(req: Request, res: Response) {
+    const userId = req.userID;
+    const { postId, id: commentId } = req.params;
+    const { description } = req.body;
 
-		if (!userExists) {
-			return response(res, 'Você não pode atualizar esse comentário!');
-		}
+    if (!userId) {
+      return response(
+        res,
+        "Você não pode deletar esse comentário! Faça o login e tente novamente",
+        403
+      );
+    }
 
-		try {
-			await prismaService.comment.update({
-				where: {
-					id: Number(id),
-					postId: Number(postId),
-					authorId: userExists.id,
-				},
-				data: {
-					description,
-				},
-			});
+    if (!postId) {
+      return response(
+        res,
+        "O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!"
+      );
+    }
 
-			res.status(200).json({
-				message: 'Comentário atualizado com sucesso',
-			});
-		} catch {
-			response(res, 'Não foi possível atualizar o comentário', 404);
-		}
-	}
+    if (!commentId) {
+      return response(
+        res,
+        "O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!"
+      );
+    }
 
-	async delete(req: Request, res: Response) {
-		const { postId, id } = req.params;
-		const { description, email } = req.body;
+    if (!description) {
+      return response(res, "Não é possível atualizar um comentário vazio");
+    }
 
-		if (!postId) {
-			return response(
-				res,
-				'O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!'
-			);
-		}
+  
 
-		if (!id) {
-			return response(
-				res,
-				'O que faz nessa rota? Seu curioso! Volte para a página inicial e tente novamente!'
-			);
-		}
+    try {
+      await prismaService.comment.delete({
+        where: {
+          id: Number(commentId),
+          postId: Number(postId),
+          authorId: userId,
+        },
+      });
 
-		if (!description) {
-			return response(
-				res,
-				'Não é possível atualizar um comentário vazio'
-			);
-		}
-
-		if (!email) {
-			return response(
-				res,
-				'Cadê sua identificação? Faça login e tente novamente!'
-			);
-		}
-
-		const userExists = await prismaService.user.findFirst({
-			where: {
-				email,
-			},
-		});
-
-		if (!userExists) {
-			return response(res, 'Você não pode atualizar esse comentário!');
-		}
-
-		try {
-			await prismaService.comment.delete({
-				where: {
-					id: Number(id),
-					postId: Number(postId),
-					authorId: userExists.id,
-				},
-			});
-
-			res.status(200).json({
-				message: 'Comentário deletado com sucesso',
-			});
-		} catch {
-			response(res, 'Não foi possível deletar o comentário', 404);
-		}
-	}
+      res.status(200).json({
+        message: "Comentário deletado com sucesso",
+      });
+    } catch {
+      response(res, "Não foi possível deletar o comentário", 404);
+    }
+  }
 }
