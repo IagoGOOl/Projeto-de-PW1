ddcc2ad4658fa64c7342c433e8ebf95ce28c7a72
PauloSantiagoADS <paulosantiago8@gmail.com>
2023-12-17 23:30:27 -0300

    Adicionando um tratamento de exceção no controlle, Atualizando controllerInstituicao,

--- Projeto PW1/src/Controllers/controllerUser.ts
+++ Projeto PW1/src/Controllers/controllerUser.ts
@@ -1,86 +1,84 @@
-import { hash } from 'bcrypt';
-import { Request, Response } from 'express';
-import { prismaService } from '../service/prismaService';
+import { hash } from "bcrypt";
+import { Request, Response } from "express";
+import { prismaService } from "../service/prismaService";
 
 export class UserController {
-	async read(req: Request, res: Response) {
-		const id = req.userID;
+  async read(req: Request, res: Response) {
+    const id = req.userID;
 
-		try {
-			const user = await prismaService.user.findUnique({
-				where: { id },
-			});
-			res.status(201).json({ user });
-		} catch (err) {
-			res.status(500).json({ message: 'Erro ao procurar por Usuário' });
-		}
-	}
+    try {
+      const user = await prismaService.user.findUnique({
+        where: { id },
+      });
+      res.status(201).json({ user });
+    } catch (err) {
+      res.status(500).json({ message: "Erro ao procurar por Usuário" });
+    }
+  }
 
-	async create(req: Request, res: Response) {
-		const { name, email, password } = req.body;
-		const encryptedPassword = await hash(password, 10);
+  async create(req: Request, res: Response) {
+    const { name, email, password } = req.body;
+    const encryptedPassword = await hash(password, 10);
 
-		try {
-			await prismaService.user.create({
-				data: {
-					name,
-					email,
-					password: encryptedPassword,
-				},
-			});
-			res.status(201).json({ message: 'Usuário Criado com sucesso' });
-		} catch (err) {
-			console.error(err);
-			res.status(500).json({ message: 'Erro ao criar o Usuário' });
-		}
-	}
-	async update(req: Request, res: Response) {
-		const { name, email, password } = req.body;
-		const id = req.userID;
+    try {
+      await prismaService.user.create({
+        data: {
+          name,
+          email,
+          password: encryptedPassword,
+        },
+      });
+      res.status(201).json({ message: "Usuário Criado com sucesso" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Erro ao criar o Usuário" });
+    }
+  }
+  async update(req: Request, res: Response) {
+    const { name, email, password } = req.body;
+    const id = req.userID;
 
-		try {
-			await prismaService.user.update({
-				where: { id },
-				data: {
-					name,
-					email,
-					password,
-				},
-			});
-			res.status(201).json({ message: 'Usuário Criado com sucesso' });
-		} catch (err) {
-			res.status(500).json({
-				message: 'Erro ao atualizar dados de Usuário',
-			});
-		}
-	}
+    try {
+      await prismaService.user.update({
+        where: { id },
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+      res.status(201).json({ message: "Usuário atualizado com sucesso" });
+    } catch (err) {
+      res.status(500).json({
+        message: "Erro ao atualizar dados de Usuário",
+      });
+    }
+  }
 
-	async delete(req: Request, res: Response) {
-		const id = req.userID;
+  async delete(req: Request, res: Response) {
+    const id = req.userID;
 
-		try {
-			await prismaService.user.delete({
-				where: { id },
-			});
-			res.status(201).json({ message: 'Usuário excluido com sucesso' });
-		} catch (err) {
-			res.status(500).json({ message: 'Erro ao excluir Usuário' });
-		}
-	}
+    try {
+      await prismaService.user.delete({
+        where: { id },
+      });
+      res.status(201).json({ message: "Usuário excluido com sucesso" });
+    } catch (err) {
+      res.status(500).json({ message: "Erro ao excluir Usuário" });
+    }
+  }
 
-	async updateImage(req: Request, res: Response) {
-		const { filename } = req?.file || {};
-		const id = req.userID;
-		const image = filename
-			? 'http://localhost:3000/images/' + filename
-			: null;
-		await prismaService.user.update({
-			where: { id },
-			data: { image },
-		});
-		res.status(201).json({
-			message: 'Imagem de perfil salva com sucesso!',
-			image,
-		});
-	}
+  async updateImage(req: Request, res: Response) {
+    const { filename } = req?.file || {};
+    const id = req.userID;
+    const image = filename ? "http://localhost:3000/images/" + filename : null;
+    await prismaService.user.update({
+      where: { id },
+      data: { image },
+    });
+    res.status(201).json({
+      message: "Imagem de perfil salva com sucesso!",
+      image,
+    });
+  }
 }
